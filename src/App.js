d6604a5ddd5f263b25411c0271d6e1cd4bbef2b9
git99.src <cpu.al2@gmail.com>
2020-11-05 20:48:18 -0800

    Added jumbtron test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="jumbotron jumbotron-fluid">
+  <div className="container">
+    <h1 className="display-4">React Confirmation</h1>
     </div>
+</div>
   );
 }
 
