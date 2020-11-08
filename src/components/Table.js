17af40b69ef3207ec967324294dfb43251a574fd
git99.src <cpu.al2@gmail.com>
2020-11-07 18:18:58 -0800

    Table data rendered and with filter

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,34 +3,45 @@ import React from "react";
 
 // Whenever we try to render an array containing JSX, React knows to render each JSX element separately
 function Table(props) {
-  return (
-    <ul className="list-group">
-      {props.employees.map(item => (
-        <li className="list-group-item" key={item.id}>
-          {item.picture} {item.first}{item.last} {item.phone}{item.email} {item.dob}
-        </li>
-      ))}
-    </ul>
+    return (
+        // <ul className="list-group">
+        //   {props.employees.map(item => (
+        //     <li className="list-group-item" key={item.id}>
+        //       {item.picture} {item.first}{item.last} {item.phone}{item.email} {item.dob}
+        //     </li>
+        //   ))}
+        // </ul>
 
-// <table className="table table-striped table-bordered">
-//   <thead>
-//     <tr>
-//       <th scope="col">Image</th>
-//       <th scope="col">First</th>
-//       <th scope="col">Last</th>
-//       <th scope="col">Phone</th>
-//       <th scope="col">Email</th>
-//       <th scope="col">DOB</th>
-//     </tr>
-//   </thead>
-//   <tbody>
-//   <div>
-//    <TableData />
-//   </div>
-//   </tbody>
-// </table>
+        <table className="table table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th scope="col">Image</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">DOB</th>
+                </tr>
+            </thead>
+            <tbody>
+              
+                    {props.employees.map(item => (
+                        <tr>
+                          
+                            <td><img alt={item.image} src={item.picture}/></td>
+                            <td>{item.first}</td>
+                            <td>{item.last}</td>
+                            <td>{item.phone}</td>
+                            <td>{item.email}</td>
+                            <td>{item.dob}</td>
+                        </tr>
+                    ))}
 
-  );
+               
+            </tbody>
+        </table>
+
+    );
 }
 
 export default Table;
